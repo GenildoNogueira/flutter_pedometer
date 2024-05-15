a52a64183ce62eda4596cb42a4c45a8397e9fae3
Genildo Nogueira <genildonogueira37@gmail.com>
2024-05-15 19:30:20 -0300

    changing class names

--- ios/Classes/FlutterPedometerPlugin.m
+++ ios/Classes/FlutterPedometerPlugin.m
@@ -1,15 +1,15 @@
-#import "PedometerPlugin.h"
-#if __has_include(<pedometer/pedometer-Swift.h>)
-#import <pedometer/pedometer-Swift.h>
+#import "FlutterPedometerPlugin.h"
+#if __has_include(<flutter_pedometer/flutter_pedometer-Swift.h>)
+#import <flutter_pedometer/flutter_pedometer-Swift.h>
 #else
 // Support project import fallback if the generated compatibility header
 // is not copied when this plugin is created as a library.
 // https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
-#import "pedometer-Swift.h"
+#import "flutter_pedometer-Swift.h"
 #endif
 
-@implementation PedometerPlugin
+@implementation FlutterPedometerPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar> *)registrar {
-  [PedometerPlugin registerWithRegistrar:registrar];
+  [SwiftPedometerPlugin registerWithRegistrar:registrar];
 }
 @end
